39d39e45e377e43810b05a5a90ed23897200a5ab
Tit Tolkachev <tittolkachev@gmail.com>
2024-02-19 22:20:20 +0700

    Init.

---
+++ app/src/main/java/com/example/bankofficer/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.example.bankofficer.di
+
+import org.koin.dsl.module
+
+
+val appModule = module {
+
+    //region Network
+    //endregion
+
+    //region DataBase
+    //endregion
+
+}
